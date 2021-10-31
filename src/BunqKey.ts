46637d89a3bccb0d0cd65eb69016ea4373252957
entttom <entner.tom@gmail.com>
2021-10-31 09:46:12 +0100

    API signing changed - sign body only

--- src/BunqKey.ts
+++ src/BunqKey.ts
@@ -1,105 +1,98 @@
-const  NodeRSA = require('node-rsa');
-import fs = require('fs-extra');
-
-export class BunqKey {
-
-    constructor(privateKeyPem:string) {
-        this.key = new NodeRSA(privateKeyPem, {signingScheme: 'pkcs1-sha256', env: 'node'});
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BunqKey = void 0;
+var NodeRSA = require('node-rsa');
+var fs = require("fs-extra");
+var BunqKey = /** @class */ (function () {
+    function BunqKey(privateKeyPem) {
+        this.key = new NodeRSA(privateKeyPem, { signingScheme: 'pkcs1-sha256', env: 'node' });
     }
-
-    static createNew():BunqKey {
+    BunqKey.createNew = function () {
         /* istanbul ignore next */
         {
-            let aKey: any = new NodeRSA({
+            var aKey = new NodeRSA({
                 b: 2048,
                 encryptionScheme: 'pkcs1',
                 signingScheme: 'pkcs1-sha256'
             });
-            let privateKeyString = aKey.exportKey('pkcs8-private-pem');
+            var privateKeyString = aKey.exportKey('pkcs8-private-pem');
             return new this(privateKeyString);
         }
-    }
-
-    static createFromPrivateKeyFile(pemFilename:string) {
-        const buffer = fs.readFileSync(pemFilename);
+    };
+    BunqKey.createFromPrivateKeyFile = function (pemFilename) {
+        var buffer = fs.readFileSync(pemFilename);
         return new this(buffer.toString());
-    }
-
-    toPublicKeyString() : string {
+    };
+    BunqKey.prototype.toPublicKeyString = function () {
         return this.key.exportKey('pkcs8-public-pem');
-    }
-
-    toPrivateKeyString() : string {
+    };
+    BunqKey.prototype.toPrivateKeyString = function () {
         return this.key.exportKey('pkcs8-private-pem');
-    }
-
-    signApiCall(options:any):any {
-        let stringToSign = this.createStringToSign(options);
+    };
+    BunqKey.prototype.signApiCall = function (options) {
+        var stringToSign = this.createStringToSign(options);
         //console.log("signApiCall:"+stringToSign);
         //let key = new NodeRSA(this.privateKey, {signingScheme: 'pkcs1-sha256', env: 'node'});
-        return this.key.sign(stringToSign,'base64','utf8');
-
+        return this.key.sign(stringToSign, 'base64', 'utf8');
         // const sign = crypto.createSign('sha256');
         // sign.update(stringToSign);
         // return sign.sign({
         //   key: this.privateKey,
         //   passphrase: ""
         // }, "base64");
-    }
-
-    verifySigWithPubkey(options):boolean {
-        let pubKey = new NodeRSA();
-        pubKey.importKey(this.toPublicKeyString(),'public');
-        let optionsSig = options.headers['X-Bunq-Client-Signature'];
+    };
+    BunqKey.prototype.verifySigWithPubkey = function (options) {
+        var pubKey = new NodeRSA();
+        pubKey.importKey(this.toPublicKeyString(), 'public');
+        var optionsSig = options.headers['X-Bunq-Client-Signature'];
         delete options.headers['X-Bunq-Client-Signature'];
-        let stringToSign = this.createStringToSign(options);
-        options.headers['X-Bunq-Client-Signature']=optionsSig;
+        var stringToSign = this.createStringToSign(options);
+        options.headers['X-Bunq-Client-Signature'] = optionsSig;
         //console.log("verify:"+stringToSign);
-        return pubKey.verify(stringToSign, optionsSig,'utf8','base64');
-    }
-
-    createStringToSign(options:any) : string {
-        let stringToSign:string = options.method + " ";
-        // let endPoint:string = options.uri;
+        return pubKey.verify(stringToSign, optionsSig, 'utf8', 'base64');
+    };
+    BunqKey.prototype.createStringToSign = function (options) {
+	var stringToSign = ""       
+       // var stringToSign = "options.method + " "";
+       /* // let endPoint:string = options.uri;
         //if(options.uri.indexOf("bunq.com") != -1)
         // endPoint = (options.uri.split("bunq.com"))[1];
         stringToSign += (options.uri.split("bunq.com"))[1];
         stringToSign += "\n";
-
         // We need to order the headers
-        const orderedHeaders = BunqKey.orderKeys(options.headers);
-        Object.keys(orderedHeaders).forEach(function(key) {
+        var orderedHeaders = BunqKey.orderKeys(options.headers);
+        Object.keys(orderedHeaders).forEach(function (key) {
             //if (key.startsWith("X-Bunq-") || key == "Cache-Control" || key == "User-Agent")
-                stringToSign += key + ": " + orderedHeaders[key] + "\n";
+            stringToSign += key + ": " + orderedHeaders[key] + "\n";
         });
         stringToSign += "\n";
+        */
         if (options.body) {
             stringToSign += options.body.toString();
+            console.log(options.body.toString())
         }
+        console.log("string to sign: " + stringToSign)
         return stringToSign;
-    }
-
+    };
     // credit to http://stackoverflow.com/questions/9658690/is-there-a-way-to-sort-order-keys-in-javascript-objects
-    private static orderKeys(obj:any):any {
-
-        const keys = Object.keys(obj).sort(function keyOrder(k1, k2) {
-            if (k1 < k2) return -1;
-            else return +1;
-
+    BunqKey.orderKeys = function (obj) {
+        var keys = Object.keys(obj).sort(function keyOrder(k1, k2) {
+            if (k1 < k2)
+                return -1;
+            else
+                return +1;
         });
-
-        const after:any = {};
-        for (let i = 0; i < keys.length; i++) {
+        var after = {};
+        for (var i = 0; i < keys.length; i++) {
             after[keys[i]] = obj[keys[i]];
             delete obj[keys[i]];
         }
-
-        for (let i = 0; i < keys.length; i++) {
+        for (var i = 0; i < keys.length; i++) {
             obj[keys[i]] = after[keys[i]];
         }
         return obj;
-    }
-
-
-    readonly key : any;
-}
+    };
+    return BunqKey;
+}());
+exports.BunqKey = BunqKey;
+//# sourceMappingURL=BunqKey.js.map
